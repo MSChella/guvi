567a944dd55bae7a7468e5240995314e4a0da6fa
MSChella <reachchellapathy.ms@gmail.com>
2024-01-02 08:57:34 +0530

    Changed th jsx inside the return as moved the looping function 2 divs starting with card

--- src/components/Main Page/MainPageCard.jsx
+++ src/components/Main Page/MainPageCard.jsx
@@ -28,11 +28,13 @@ const MainPageCard = ({ updateCartQuantity }) => {
 
 
     return (<>
-        {products.map((product) => (
 
-            <div className="container px-4 px-lg-5 mt-5" key={product.id}>
-                <div className="row gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-4 justify-content-center">
-                    <div className="col mb-5">
+
+        <div className="container px-4 px-lg-5 mt-5" >
+            <div className="row gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-4 justify-content-center">
+
+                {products.map((product) => (
+                    <div className="col mb-5" key={product.id}>
                         <div className="card h-100">
 
                             <img className="card-img-top" src="https://dummyimage.com/450x300/dee2e6/6c757d.jpg" alt="..." />
@@ -51,13 +53,13 @@ const MainPageCard = ({ updateCartQuantity }) => {
                                 </div>
                             </div>
                         </div>
-                    </div>
-                </div>
+                    </div>))}
             </div>
+        </div>
 
 
 
-        ))} </>
+    </>
     )
 
 
