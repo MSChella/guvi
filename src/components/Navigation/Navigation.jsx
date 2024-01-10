3a3964ae16918b06ef0f2f98408668df74b49f39
MSChella <reachchellapathy.ms@gmail.com>
2024-01-10 23:28:35 +0530

    Updated the Nav cart item with props for cartquantity

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -32,7 +32,7 @@ const Navigation = ({ cartQuantity }) => {
                         <button className="btn btn-outline-dark" type="submit">
                             <i className="bi-cart-fill me-1"></i>
                             Cart
-                            <span className="badge bg-dark text-white ms-1 rounded-pill">0</span>
+                            <span className="badge bg-dark text-white ms-1 rounded-pill">{cartQuantity}</span>
                         </button>
                     </form>
                 </div>
